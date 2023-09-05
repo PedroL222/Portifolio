a6eb9905099737f3f06e9e26a822873c7c65888a
Pedro Lucas Freitas Sousa <Pedrofreitas16083@gmail.com>
2023-09-05 11:40:00 -0300

    Fazendo o Menu ficar Responsivo e ícones de redes sociais

---
+++ static/script.js
@@ -0,0 +1,11 @@
+    // Abrir e fechar menu lateral em celulares
+
+    const menuMobile = document.querySelector('.menu-mobile');
+    const body = document.querySelector('body');
+
+    menuMobile.addEventListener('click', ()=>{
+        menuMobile.classList.contains('bi-list')
+        ? menuMobile.classList.replace("bi-list","bi-x")
+        : menuMobile.classList.replace("bi-x","bi-list")
+        body.classList.toggle("menu-nav-active")
+    });
