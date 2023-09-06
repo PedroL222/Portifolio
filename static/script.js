39de97f91374365428801f3c0d4894be4c1f20bd
Pedro Lucas Freitas Sousa <Pedrofreitas16083@gmail.com>
2023-09-06 17:42:16 -0300

    Terminando section Sobre e começando section Habilidades

--- static/script.js
+++ static/script.js
@@ -1,11 +1,24 @@
     // Abrir e fechar menu lateral em celulares
 
-    const menuMobile = document.querySelector('.menu-mobile');
-    const body = document.querySelector('body');
+const menuMobile = document.querySelector('.menu-mobile');
+const body = document.querySelector('body');
 
-    menuMobile.addEventListener('click', ()=>{
-        menuMobile.classList.contains('bi-list')
-        ? menuMobile.classList.replace("bi-list","bi-x")
-        : menuMobile.classList.replace("bi-x","bi-list")
-        body.classList.toggle("menu-nav-active")
-    });
+menuMobile.addEventListener('click', ()=>{
+    menuMobile.classList.contains('bi-list')
+    ? menuMobile.classList.replace("bi-list","bi-x")
+    : menuMobile.classList.replace("bi-x","bi-list")
+    body.classList.toggle("menu-nav-active")
+});
+
+// Fechar menu ao selecionar navegação
+
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach(item =>{
+    item.addEventListener("click", () => {
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active");
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
