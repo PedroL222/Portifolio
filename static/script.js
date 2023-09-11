977106c88d166ec0e73ca901386fef285be27cc3
Pedro Lucas Freitas Sousa <Pedrofreitas16083@gmail.com>
2023-09-11 11:05:32 -0300

    Colocando animações

--- static/script.js
+++ static/script.js
@@ -22,3 +22,25 @@ navItem.forEach(item =>{
         }
     })
 })
+
+// Animar tudo que tiver data-animação
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+
+  item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+  animeScroll();
+})
